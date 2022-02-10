e48a44c7ac25065d9103ed8e45e05b4888e79b92
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-10 17:59:49 -0300

    create remove_task_use_case_test.dart

--- test/domain/use_case/add_task_use_case_test.dart
+++ test/domain/use_case/add_task_use_case_test.dart
@@ -8,11 +8,18 @@ void main() {
     addTaskUseCase = AddTaskUseCase();
   });
   group('GIVEN a call on add', () {
-    test('WHEN typedTask is blank THEN it should return task list length = 2',
+    test(
+        'WHEN typedTask is not empty THEN it should return task list length = 2',
         () async {
       final tasks = <Task>[Task(description: 'task 1')];
-      final taskList = addTaskUseCase.add('typedTask', tasks);
+      final taskList = addTaskUseCase.add('task 2', tasks);
       expect(taskList.length, 2);
+    });
+    test('WHEN typedTask is empty THEN it should return task list length = 1',
+        () async {
+      final tasks = <Task>[Task(description: 'task 1')];
+      final taskList = addTaskUseCase.add('', tasks);
+      expect(taskList.length, 1);
     });
   });
 }
--- test/domain/use_case/remove_task_use_case_test.dart
+++ test/domain/use_case/remove_task_use_case_test.dart
@@ -1,9 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:todolistapp/domain/model/task.dart';
+import 'package:todolistapp/domain/use_case/remove_task_use_case.dart';
 
 void main() {
-  setUpAll(() {});
-  group('GIVEN a call on validatePassword', () {
-    test('WHEN password is blank THEN it should return an PasswordStatus.empty',
-        () async {});
+  late RemoveTaskUseCase removeTaskUseCase;
+  setUpAll(() {
+    removeTaskUseCase = RemoveTaskUseCase();
+  });
+  group('GIVEN a call on remove', () {
+    test('THEN it should return an task list length = 1', () async {
+      final tasks = <Task>[
+        Task(description: 'task 1'),
+        Task(description: 'task 2'),
+      ];
+      final taskList = removeTaskUseCase.remove(1, tasks);
+      expect(taskList.length, 1);
+    });
   });
 }
