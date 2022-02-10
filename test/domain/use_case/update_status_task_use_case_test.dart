8f9864d386df34d10056cd1f90d791618930810d
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-10 18:06:00 -0300

    create update_status_task_use_case_test.dart

--- test/domain/use_case/update_status_task_use_case_test.dart
+++ test/domain/use_case/update_status_task_use_case_test.dart
@@ -1,9 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:todolistapp/domain/model/task.dart';
+import 'package:todolistapp/domain/use_case/update_status_task_use_case.dart';
 
 void main() {
-  setUpAll(() {});
-  group('GIVEN a call on validatePassword', () {
-    test('WHEN password is blank THEN it should return an PasswordStatus.empty',
-        () async {});
+  late UpdateStatusTaskUseCase updateStatusTaskUseCase;
+  setUpAll(() {
+    updateStatusTaskUseCase = UpdateStatusTaskUseCase();
+  });
+  group('GIVEN a call on update', () {
+    test(
+        'WHEN completed attribute index 0 of tasks = false '
+        'THEN it should return completed attribute index 0 of tasks = true',
+        () async {
+      final tasks = <Task>[
+        Task(description: 'task 1', completed: false),
+        Task(description: 'task 2'),
+      ];
+      final taskList = updateStatusTaskUseCase.update(0, true, tasks);
+      expect(taskList[0].completed, true);
+    });
   });
 }
